9c0a607a0b719dcf3e58c17b3cca6904070acbe3
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-06-11 23:26:03 +0600

    Add Jwt

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT | 5000
 
 app.use(cors());
 app.use(express.json())
+
+const verifyAccessWithJwtToken = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+
+    const accessToken = authorization.split(' ')[1];
+
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ddbcqih.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,6 +48,12 @@ async function run() {
         const instructorCollection = client.db('sportsZone').collection('instructors');
         const selectedClassCollection = client.db('sportsZone').collection('selectedclass');
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ accessToken });
+        })
+
         // users Collection
 
         app.post('/users', async (req, res) => {
@@ -52,32 +76,32 @@ async function run() {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
-              $set: {
-                role: 'admin'
-              }
+                $set: {
+                    role: 'admin'
+                }
             }
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result);
-          })
+        })
 
         app.patch('/users/instructor/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
-              $set: {
-                role: 'instructor'
-              }
+                $set: {
+                    role: 'instructor'
+                }
             }
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result);
-          })
+        })
 
-          app.delete('/users/admin/:id', async (req, res) => {
+        app.delete('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await usersCollection.deleteOne(query);
             res.send(result)
-          })
+        })
 
         // classes collection 
 
@@ -101,10 +125,14 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/selectedclass', async (req, res) => {
+        app.get('/selectedclass', verifyAccessWithJwtToken, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([])
+            }
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' });
             }
             const query = { email: email }
             const result = await selectedClassCollection.find(query).toArray();
@@ -116,7 +144,7 @@ async function run() {
             const query = { _id: new ObjectId(id) };
             const result = await selectedClassCollection.deleteOne(query);
             res.send(result)
-          })
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
